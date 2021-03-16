2f90111438320ace87169ceb89829385c27bd2b1
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-03-15 22:31:07 -0400

    Added API integration and installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Weather />
       <Footer />
     </div>
   );
