0a570c105437bc6b4cd1e71d045439b93246c78e
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-03-15 22:01:05 -0400

    Added a footer with the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
+import Footer from "./Footer";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Footer />
+    </div>
+  );
 }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <small>
+        <a
+          href="https://github.com/madtos12/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Madeline Santos
+      </small>
+    </div>
+  );
+}
