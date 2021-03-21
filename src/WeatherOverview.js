f9c61889c05cbfcaf378dde83295aad5a7cc05b6
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-03-21 15:23:09 -0400

    Updated file name; updating column sizes; added rounding to wind

--- src/WeatherOverview.js
+++ src/WeatherOverview.js
@@ -3,18 +3,18 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeatherInfo(props) {
+export default function WeatherOverview(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
-        <li>
+        <li className="current-date">
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}</li>
+        <li className="weather-description">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-7">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} />
@@ -25,10 +25,10 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-5">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
           </ul>
         </div>
       </div>
