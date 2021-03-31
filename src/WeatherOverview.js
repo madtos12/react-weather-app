6b2424084b32291d73c0d562c30ff4c1166042bf
madtos12 <78381102+madtos12@users.noreply.github.com>
2021-03-30 21:36:04 -0400

    Changed col sizing and classnames to humidity and wind

--- src/WeatherOverview.js
+++ src/WeatherOverview.js
@@ -16,26 +16,25 @@ export default function WeatherOverview(props) {
         <li className="weather-description">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-7">
+        <div className="col-8">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} />
             </div>
-
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
-        <div className="col-5">
+        <div className="col-4">
           <ul>
             <li>
               <img className="humidity-icon" src={Humidity} alt="humidity" />
-              <span className="humidity"> {props.data.humidity}%</span>
+              <span className="humidity">{props.data.humidity}%</span>
             </li>
             <li>
               <img className="wind-icon" src={Wind} alt="wind" />
-              <span className="wind"> {Math.round(props.data.wind)} km/h</span>
+              <span className="wind">{Math.round(props.data.wind)} km/h</span>
             </li>
           </ul>
         </div>
